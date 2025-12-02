050dd64c61bc14fdff9136e982245b894af8a92b
Adam Siemieniuk <adam.siemieniuk@intel.com>
2025-12-02 11:31:55 +0100

    [utils] Math equations evaluator

--- ai_bench/utils/__init__.py
+++ ai_bench/utils/__init__.py
@@ -1,9 +1,13 @@
+from .equations import eval_ast
+from .equations import eval_eq
 from .finder import kernel_bench_dir
 from .finder import project_root
 from .finder import specs
 from .importer import import_from_path
 
 __all__ = [
+    "eval_ast",
+    "eval_eq",
     "import_from_path",
     "kernel_bench_dir",
     "project_root",
