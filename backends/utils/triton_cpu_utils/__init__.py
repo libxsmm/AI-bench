2bb55bc58fc2d1254d44757ee6d40990a27ed0e4
Julian Oppermann <julian.oppermann@intel.com>
2026-07-03 01:40:46 -0700

    Pre-commit

--- backends/utils/triton_cpu_utils/__init__.py
+++ backends/utils/triton_cpu_utils/__init__.py
@@ -1,7 +1,8 @@
 """Helper utilities for AI-bench Triton CPU kernels."""
 
 from .gilbert_d2xy import gilbert_d2xy
-from .sfc_matmul import pack_weights_for_sfc_matmul, sfc_matmul
+from .sfc_matmul import pack_weights_for_sfc_matmul
+from .sfc_matmul import sfc_matmul
 
 __all__ = [
     "gilbert_d2xy",
